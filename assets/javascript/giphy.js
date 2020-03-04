ef6d3cb2928314e60b8e86f1e2ee1710bd006526
cshanebrown78 <cshanebrown78@gmail.com>
2020-03-04 11:32:33 -0600

    Added new button coding to html file

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,4 +1,6 @@
-var topics = ["airplane", "helicopter", "spaceship", "glider", "f-16", "f-22", "f-35", "p51 Mustang", "cessna", "cirrus", "boeing", "falcon-9", "starship", "falcon heavy"];
+$(document).ready(function() {
+
+var topics = ["airplane", "helicopter", "spaceship", "glider", "f-16", "f-22", "f-35", "p51 Mustang", "cessna", "cirrus sr22", "boeing", "falcon-9", "starship", "falcon heavy"];
 
 for (var i = 0; i < topics.length; i++) {
     aircraftBtn = $("<button type='button' class='btn btn-primary aircraft'>")
@@ -8,6 +10,7 @@ for (var i = 0; i < topics.length; i++) {
 }
 
 $(".aircraft").on("click", function() {
+    $(".gifs").empty();
     var aircraft = $(this).attr("data-aircraft");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
       aircraft + "&api_key=yWGuBvlaHmB4UaZ5SAJlstG7bG6D5gMN&limit=10";
@@ -51,4 +54,5 @@ $(".aircraft").on("click", function() {
         });
 
     });
+});
 });
